d89be09d74d1d8232e0001af9c44eef03bdf443a
Josh Baker <joshbaker@MacBook-Air.lan>
2025-07-28 19:29:17 -0400

    added nvim files

---
+++ nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "lewpoly/sherbet.nvim",
+  },
+
+  {
+    "AlexvZyl/nordic.nvim",
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = Colorscheme,
+    },
+  },
+}
