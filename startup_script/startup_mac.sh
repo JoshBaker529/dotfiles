7770b7396ac8e46eab0e7472ecf15bc44e1f1485
Josh Baker <JoshBaker529@Gmail.com>
2025-07-29 18:49:32 -0400

    Added tmux session starting and yabai config

--- startup_script/startup_mac.sh
+++ startup_script/startup_mac.sh
@@ -5,3 +5,29 @@
 
 # Starts yabai, window manager
 yabai --start-service
+
+yabai -m signal --add event=dock_did_restart action="sudo yabai --load-sa"
+sudo yabai --load-sa
+
+# .. more yabai startup stuff
+
+# Sets the layout to be bsp.
+# Alternative options: float
+# bsp: Tiling windows, no overlap
+# float: similar to default
+yabai -m config layout bsp
+
+# Set all padding and gaps to 20pt (default: 0)
+yabai -m config top_padding 10
+yabai -m config bottom_padding 10
+yabai -m config left_padding 10
+yabai -m config right_padding 10
+yabai -m config window_gap 10
+
+# Window Opacity
+yabai -m config window_opacity on
+yabai -m config active_window_opacity 0.9
+#yabai -m config normal_window_opacity 0.90
+
+# Start a Tmux session named main, or join the main session if it already exists
+tmux new-session -A -s main || tmux attach-session -t main
