a4356ad428625532a568cf78fa4331c5741076cc
Josh Baker <JoshBaker529@Gmail.com>
2025-07-28 22:41:50 -0400

    Changed mv to cp

--- scripts/mac_install.sh
+++ scripts/mac_install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ -d "$HOME/.config/" ]; then
-  mv "../nvim/" "$HOME/.config/nvim/"
+  cp "../nvim/" "$HOME/.config/nvim/"
   echo "Copied Neovim successfully"
 else
   echo "Creating ~/.config/  ..."
   mkdir "$HOME/.config"
   echo "Done."
-  mv "../nvim/" "~/.config/nvim/"
+  cp "../nvim/" "~/.config/nvim/"
   echo "Copied Neovim successfully"
 
 fi
