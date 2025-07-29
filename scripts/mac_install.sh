9f3a882d311c5b4396cc18d5cee211e8f5bc66dc
Josh Baker <JoshBaker529@Gmail.com>
2025-07-28 22:47:17 -0400

    Added tmux and yabai copying

--- scripts/mac_install.sh
+++ scripts/mac_install.sh
@@ -2,12 +2,18 @@
 
 if [ -d "$HOME/.config/" ]; then
   cp "../nvim/" "$HOME/.config/nvim/"
-  echo "Copied Neovim successfully"
+  echo "Copied neovim successfully."
 else
   echo "Creating ~/.config/  ..."
   mkdir "$HOME/.config"
   echo "Done."
   cp "../nvim/" "~/.config/nvim/"
   echo "Copied Neovim successfully"
-
 fi
+
+cp "../tmux/.tmux.conf" "$HOME/"
+echo "Copied tmux config successfully."
+
+cp "../yabai/.yabairc" "$HOME/"
+
+echo "Copied yabai config successfully."
