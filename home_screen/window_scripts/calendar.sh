5c439c26b5c66f38999cc4283200f9d2a4e2e78e
Josh Baker <JoshBaker529@Gmail.com>
2025-08-20 19:05:50 -0400

    Replaced scripts with a separate project

--- home_screen/window_scripts/calendar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This file simply prints a calendar, and update it occasionally
-
-while :; do
-  clear
-  cal
-  sleep 1h
-done
--- home_screen/window_scripts/time.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This file simply prints the time over and over
-
-while :; do
-  clear
-  date +'%a %b %e %Y%n%I:%M %p'
-  sleep 15s
-done
