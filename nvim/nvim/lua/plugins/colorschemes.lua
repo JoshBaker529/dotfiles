e020d0dc700fd42024308e1985c49823922f81ea
Josh Baker <JoshBaker529@Gmail.com>
2025-08-20 18:48:42 -0400

    Changed themes to everblush

---
+++ nvim/nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "lewpoly/sherbet.nvim",
+  },
+
+  {
+    "AlexvZyl/nordic.nvim",
+  },
+
+  {
+    "Everblush/nvim",
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = Colorscheme,
+    },
+  },
+}
